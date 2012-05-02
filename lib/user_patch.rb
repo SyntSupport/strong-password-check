92035a8e716828cd1287078924a54ccb4ecbfa41
Ilya Turkin <iturkin@syntellect.ru>
2012-05-02 11:47:30 +0400

    Second commit

--- lib/user_patch.rb
+++ lib/user_patch.rb
@@ -15,16 +15,15 @@ module StrongPasswordCheck
       end
 
       module InstanceMethods   
-        # when updating an issues due_date     
         def before_save_with_pass_check
             if self.password_confirmation
               if self.password.match(/^#{self.login}/)
-                self.errors.add_to_base(l(:message_your_pass, :pass => self.password, :points => 0, :log => l(:start_with_name)))
+                self.errors.add_to_base(l(:message_your_pass, :log => l(:start_with_name)))
                 return false
               end
-              isstrongenough, points, strlog = strong_enough_pass(self.password)
+              isstrongenough, strlog = strong_enough_pass(self.password)
               if !isstrongenough
-                self.errors.add_to_base(l(:message_your_pass, :pass => self.password, :points => points, :log => strlog)[0..-3])
+                self.errors.add_to_base(l(:message_your_pass, :log => strlog)[0..-3])
                 return false
               end
             end
@@ -32,83 +31,109 @@ module StrongPasswordCheck
         end
 
         def strong_enough_pass (passwd)
-            intscore   = 0
-            #strVerdict = "weak"
-            strlog     = ""
-
-            if (passwd.length<5)                         # length 4 or less
-              intscore = (intscore+3)
-              strlog   = strlog + l(:points_length_3, :length => passwd.length) + ", "
-            elsif (passwd.length>4 && passwd.length<8) # length between 5 and 7
-              intscore = (intscore+6)
-              strlog   = strlog + l(:points_length_6, :length => passwd.length) + ", "
-            elsif (passwd.length>7 && passwd.length<16) # length between 8 and 15
-              intscore = (intscore+12)
-              strlog   = strlog + l(:points_length_12, :length => passwd.length) + ", "
-            elsif (passwd.length>15)                    # length 16 or more
-              intscore = (intscore+18)
-              strlog   = strlog + l(:points_length_18, :length => passwd.length) + ", "
+            strlog = ""
+            ret = true
+            unless (/[a-z]/.match passwd)                              # [verified] at least one lower case letter
+              strlog   = strlog + l(:no_lowercase) + ", "
+              ret = false
             end
 
-
-            # LETTERS (Not exactly implemented as dictacted above because of my limited understanding of Regex)
-            if (/[a-z]/.match passwd)                              # [verified] at least one lower case letter
-              intscore = (intscore+1)
-              strlog   = strlog + l(:points_lowercase) + ", "
-            end
-
-            if (/[A-Z]/.match passwd)                              # [verified] at least one upper case letter
-              intscore = (intscore+5)
-              strlog   = strlog + l(:points_capital) + ", "
+            unless (/[A-Z]/.match passwd)                              # [verified] at least one upper case letter
+              strlog   = strlog + l(:no_capital) + ", "
+              ret = false
             end
 
             # NUMBERS
-            if (/\d+/.match passwd)                                 # [verified] at least one number
-              intscore = (intscore+5)
-              strlog   = strlog + l(:points_number) + ", "
-            end
-
-            if (/(.*[0-9].*[0-9].*[0-9])/.match passwd)             # [verified] at least three numbers
-              intscore = (intscore+5)
-              strlog   = strlog + l(:points_3_number) + ", "
+            unless (/\d+/.match passwd)                                 # [verified] at least one number
+              strlog   = strlog + l(:no_numbers) + ", "
+              ret = false
             end
 
             # SPECIAL CHAR
-            if (/.[!,@,#,$,%,^,&,*,?,_,~]/.match passwd)            # [verified] at least one special character
-              intscore = (intscore+5)
-              strlog   = strlog +  l(:points_spec) + ", "
+            unless (/.[!,@,#,$,%,^,&,*,?,_,~]/.match passwd)             # [verified] at least one spec
+              strlog   = strlog + l(:no_spec) + ", "
+              ret = false
             end
-
-            # [verified] at least two special characters
-            if (/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/.match passwd)
-              intscore = (intscore+5)
-              strlog   = strlog + l(:points_2_spec) + ", "
-            end
-
-
-            # COMBOS
-            if (/([a-z].*[A-Z])|([A-Z].*[a-z])/.match passwd)        # [verified] both upper and lower case
-              intscore = (intscore+2)
-              strlog   = strlog + l(:points_capital_lower) + ", "
-            end
-
-            if (/([a-zA-Z])/.match(passwd) && /([0-9])/.match(passwd)) # [verified] both letters and numbers
-              intscore = (intscore+2)
-              strlog   = strlog + l(:points_letters_numbers) + ", "
-            end
-
-            # [verified] letters, numbers, and special characters
-            if (/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/.match passwd)
-              intscore = (intscore+2)
-              strlog   = strlog + l(:points_numbers_spec) + ", "
-            end
-
-            ret = false
-            if (intscore > 24)
-              ret = true
-            end
-            return ret, intscore, strlog
-          end
+            return ret, strlog
+        end
+#        def strong_enough_pass (passwd)
+#            intscore   = 0
+#            #strVerdict = "weak"
+#            strlog     = ""
+#
+#            if (passwd.length<5)                         # length 4 or less
+#              intscore = (intscore+3)
+#              strlog   = strlog + l(:points_length_3, :length => passwd.length) + ", "
+#            elsif (passwd.length>4 && passwd.length<8) # length between 5 and 7
+#              intscore = (intscore+6)
+#              strlog   = strlog + l(:points_length_6, :length => passwd.length) + ", "
+#            elsif (passwd.length>7 && passwd.length<16) # length between 8 and 15
+#              intscore = (intscore+12)
+#              strlog   = strlog + l(:points_length_12, :length => passwd.length) + ", "
+#            elsif (passwd.length>15)                    # length 16 or more
+#              intscore = (intscore+18)
+#              strlog   = strlog + l(:points_length_18, :length => passwd.length) + ", "
+#            end
+#
+#
+#            # LETTERS (Not exactly implemented as dictacted above because of my limited understanding of Regex)
+#            if (/[a-z]/.match passwd)                              # [verified] at least one lower case letter
+#              intscore = (intscore+1)
+#              strlog   = strlog + l(:points_lowercase) + ", "
+#            end
+#
+#            if (/[A-Z]/.match passwd)                              # [verified] at least one upper case letter
+#              intscore = (intscore+5)
+#              strlog   = strlog + l(:points_capital) + ", "
+#            end
+#
+#            # NUMBERS
+#            if (/\d+/.match passwd)                                 # [verified] at least one number
+#              intscore = (intscore+5)
+#              strlog   = strlog + l(:points_number) + ", "
+#            end
+#
+#            if (/(.*[0-9].*[0-9].*[0-9])/.match passwd)             # [verified] at least three numbers
+#              intscore = (intscore+5)
+#              strlog   = strlog + l(:points_3_number) + ", "
+#            end
+#
+#            # SPECIAL CHAR
+#            if (/.[!,@,#,$,%,^,&,*,?,_,~]/.match passwd)            # [verified] at least one special character
+#              intscore = (intscore+5)
+#              strlog   = strlog +  l(:points_spec) + ", "
+#            end
+#
+#            # [verified] at least two special characters
+#            if (/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/.match passwd)
+#              intscore = (intscore+5)
+#              strlog   = strlog + l(:points_2_spec) + ", "
+#            end
+#
+#
+#            # COMBOS
+#            if (/([a-z].*[A-Z])|([A-Z].*[a-z])/.match passwd)        # [verified] both upper and lower case
+#              intscore = (intscore+2)
+#              strlog   = strlog + l(:points_capital_lower) + ", "
+#            end
+#
+#            if (/([a-zA-Z])/.match(passwd) && /([0-9])/.match(passwd)) # [verified] both letters and numbers
+#              intscore = (intscore+2)
+#              strlog   = strlog + l(:points_letters_numbers) + ", "
+#            end
+#
+#            # [verified] letters, numbers, and special characters
+#            if (/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/.match passwd)
+#              intscore = (intscore+2)
+#              strlog   = strlog + l(:points_numbers_spec) + ", "
+#            end
+#
+#            ret = false
+#            if (intscore > 24)
+#              ret = true
+#            end
+#            return ret, intscore, strlog
+#          end
       end
     end
   end
